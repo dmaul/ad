8abddaa39b1d9c6be6700cfd0c53d7a2a774d4e7
dmaul <jesus.jz@gmail.com>
2017-09-14 16:46:51 -0300

    Useful formulas

--- cpp/util.hpp
+++ cpp/util.hpp
@@ -14,7 +14,7 @@ rho_d: freestream density
 
 V_d: freestream velocity
 
-freestream dynamic pressure: q_d ~ 1/2(rho_d * exp(V_d, 2))
+freestream dynamic pressure: q_d ~ 1/2 * (rho_d * exp(V_d, 2))
 
 lift coefficient: CL ~ L / (q_d * S)
 
@@ -22,20 +22,27 @@ drag coefficient: CD ~ D / (q_d * S)
 
 stall speed (VStall): sqrt(W/(CL * 1/2 * rho * S))
 
-rotation velocity: 1.2 * VStall
+rotation velocity (VR): 1.2 * VStall
 
 acceleration: dV/dt = (T - D - F) / m
 
+average acceleration point: V = VR / sqrt(2)
+
+distance from V0 to VR: s1 = 1/2 * (exp(VR, 2)/a)
+
+obstacle: 35 ft 
+
+distancia from VR to obstacle clearance point : s2 = 35ft/tan(alfa)
+
+
+
+
+
+
 **/
 
-/**
-acceleration: dV/dt = (T - D - F) / m
-**/
+
 extern double accel(double T, double D, double F, double m);
 
 
-/**
-
-VStall = sqrt(W/(CL * 1/2 * rho * S)) 
-**/
 extern double vstall(double aangle, double W, double rho, double S);
