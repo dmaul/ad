3e861b1ba25f312524d4652b682f29fb045d70e3
dmaul <jesus.jz@gmail.com>
2017-09-15 16:28:00 -0300

    .

--- cpp/util.cpp
+++ cpp/util.cpp
@@ -1,15 +1,27 @@
 #include "util.hpp"
 
+double pressure(double temp)
+{
+  double T = temp;
+  return 101.29 * [(T + 273.1) / 288.08] ^ 5.256;
+}
+
 double accel(double T, double D, double F, double m)
 {
   return ( T - D - F ) / m
 }
 
-double vstall(double aangle, double W, double rho, double S)
+double vstall(double aangle, double W, double temp, double S, double V)
 {
-  double L;
-  L = N * cos(alfa) - A * sin(alfa);
-  double vstall = 0;
-  vstall = sqrt(W/(CL * 1/2 * ro * S));
+  // L = N * cos(alfa) - A * sin(alfa)
+  // N: Calculate the normal force component of aerodynamic force R
+  // A: Calculate the axial force component of aerodynamic force R
+  // CL = L / (A * .5 * r * V^2)
+   
+  double L=0, N=0, A=0, CL=0;
+  double p = pressure(temp);
+  L = N * cos(aangle) - A * sin(aangle);
+  CL = L / (S * .5 * p * exp(V, 2));
+  return sqrt(W/(CL * .5 * p * S));
 }
 
--- cpp/util.hpp
+++ cpp/util.hpp
@@ -6,7 +6,15 @@ L = N * cos(alfa) - A * sin(alfa)
 
 D = N * sin(alfa) + A * cos(alfa)
 
-S: reference aerea
+W: Weight
+
+T: Thrust of propulsion system
+
+D: Aerodynamic drag
+
+F: Rolling resistance friction
+
+S: reference area
 
 l: reference length
 
@@ -14,9 +22,15 @@ rho_d: freestream density
 
 V_d: freestream velocity
 
+A: wing aerea
+
 freestream dynamic pressure: q_d ~ 1/2 * (rho_d * exp(V_d, 2))
 
-lift coefficient: CL ~ L / (q_d * S)
+The quantity one half the density times the velocity squared is called the dynamic pressure q. So q = .5 * r * V^2
+
+lift coefficient: 
+ CL ~ L / (q_d * S)
+ CL = L / (A * .5 * r * V^2) = L / (A * q) 
 
 drag coefficient: CD ~ D / (q_d * S)
 
@@ -30,12 +44,17 @@ average acceleration point: V = VR / sqrt(2)
 
 distance from V0 to VR: s1 = 1/2 * (exp(VR, 2) / a)
 
-obstacle: 35 ft 
+obstacle: 35 ft = 10.668 meter
 
 distance from VR to obstacle clearance point : s2 = 35ft / tan(alfa)
 
 
+https://www.grc.nasa.gov/www/k-12/airplane/atmosmet.html
 
+h : altitude in meters
+Temp(Temperature): Temp = 15.04 - 0.00649 * h
+
+p(pressure): p = 101.29 * [(Temp + 273.1) / 288.08] ^ 5.256
 
 
 
@@ -44,5 +63,6 @@ distance from VR to obstacle clearance point : s2 = 35ft / tan(alfa)
 
 extern double accel(double T, double D, double F, double m);
 
-
 extern double vstall(double aangle, double W, double rho, double S);
+
+extern double pressure();
